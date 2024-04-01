5da09c5eabd1fce537f258c847524b61163d8037
PMK744 <paytonmk744@outlook.com>
2024-03-31 21:45:38 -0500

    added yarn

--- addon/scripts/index.js
+++ addon/scripts/index.js
@@ -6,15 +6,31 @@ import { HttpRequest, HttpRequestMethod, http } from "@minecraft/server-net"
 //   states.push(type.id)
 // }
 
+const arr = []
+
 const type = BlockTypes.getAll()[0]
 
 const permutation = BlockPermutation.resolve(type.id)
 
 // states.push(permutation.getAllStates())
 
-const states = BlockStates.get("direction").validValues
+const states = permutation.getAllStates()
+
+// We want map every possible state and push to the array
+for (const entry of Object.keys(states)) {
+  const state = BlockStates.get(entry).validValues
+  arr.push(state)
+}
+
+const main = []
+
+for (const entry of arr) {
+  for (const state of entry) {
+    main.push(state)
+  }
+}
 
 let request = new HttpRequest("http://localhost:8080");
 request.setMethod(HttpRequestMethod.Post);
-request.setBody(JSON.stringify(states));
+request.setBody(JSON.stringify(main));
 http.request(request);
